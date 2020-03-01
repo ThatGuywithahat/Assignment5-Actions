e03d3ae0c92d9b343a9313e88d53697086b54e86
ThatGuywithahat <Hillchar@oregonstate.edu>
2020-03-01 10:28:48 -0800

    Problem 1: Add a function that returns a radius and a test that makse sure the function works.

--- .github/workflows/task.py
+++ .github/workflows/task.py
@@ -1,7 +1,17 @@
 import math
+
+
 def firstrun():
     return "success"
 
 
 def radius(C):
     return C / (2 * math.pi)
+
+
+def getlist(list):
+    size = len(list) - 1
+    first = "first"
+    last = "last"
+    result = "First:{0} Last:{1}".format(list[0],list[size])
+    return result
--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -2,20 +2,26 @@ import unittest
 import task
 import math
 
+
 class TestCase(unittest.TestCase):
 
     def test1(self):
-        expected  = "success"
-        self.assertEqual(expected,task.firstrun())
+        expected = "success"
+        self.assertEqual(expected, task.firstrun())
 
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
     def test_radius(self):
-            C = 60
-            expected =  C/(2*math.pi)
-            self.assertEqual(expected, task.radius(C))
+        C = 60
+        expected = C / (2 * math.pi)
+        self.assertEqual(expected, task.radius(C))
+
+    def test_list(self):
+        list = [1, 2, 3]
+        expected = "First:1 Last:3"
+        self.assertEqual(expected, task.getlist(list))
 
 
 if __name__ == '__main__':
